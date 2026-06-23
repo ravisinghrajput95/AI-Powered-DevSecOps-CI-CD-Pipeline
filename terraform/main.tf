8fed1bda160603e6259206f0b726c1d1c59bd0d9
Ravi Rajput <ravisinghrajput005@gmail.com>
2026-06-23 11:37:08 +0530

    added TF code for GCP

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,7 +41,7 @@ resource "google_compute_firewall" "ssh_open" {
   source_ranges = ["0.0.0.0/0"]
 }
 
-resource "google_container_cluster" "cloudcart-dev" {
+resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.zone
 
